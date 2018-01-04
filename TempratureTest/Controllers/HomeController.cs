7b55a18124899714fcd6b1a4ea63166fcb52d175
Deathnote.1122 <Deathnote.1122@outlook.com>
2018-01-04 11:30:42 +0100

    Fever done

--- TempratureTest/Controllers/HomeController.cs
+++ TempratureTest/Controllers/HomeController.cs
@@ -1,22 +1,19 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TemperatureTest.Models;
 
-namespace TempratureTest.Controllers
+namespace TemperatureTest.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
             return View();
         }
 
@@ -26,5 +23,52 @@ namespace TempratureTest.Controllers
 
             return View();
         }
+        // GET: Home
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+
+        public ViewResult RsvpForm(Person person)
+        {
+            
+            if (ModelState.IsValid)
+                {
+                ViewBag.TestMessage = Person.Theresult(person.Temperature);
+                return View("Result", person);
+                //    if (person.Temperature == 37)
+                //    {
+                //        ViewBag.TestMessage = "Great!you are ok ,And your temperature is normal.";
+                //    }
+                //    else if (person.Temperature < 37)
+                //    {
+                //        ViewBag.TestMessage = "Your temperature is not low ,you have to visit the doctor.";
+                //    }
+                //    else
+                //    {
+                //        ViewBag.TestMessage = "Your temperature is not high ,you have to visit the doctor.";
+                //    }
+                //    return View("Result", person);
+                }
+            else
+            {
+                return View();
+            }
+            // -----------------------------------------
+            //if (ModelState.IsValid)
+            //{
+            //    return View("Result", person);
+            //}
+            //else
+            //{
+            //    return View();
+            //}
+
+        }
+
     }
 }
---
+++ TempratureTest/Models/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TemperatureTest.Models
+{
+    public class Person
+    {
+        [Required(ErrorMessage = "please enter your name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "please enter your email")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "pleas enter invaled email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "please enter your phone")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "The input must be anummber")]
+        public  int Temperature { get; set; }
+
+
+        public static string Theresult(int temperature)
+        {
+            if (temperature == 37)
+            {
+                return "Great!you are ok ,And your temperature is normal.";
+            }
+            else if (temperature > 37)
+            {
+                return "Your temperature is  high ,you have to visit the doctor.";
+            }
+            else
+            {
+                 return "Your temperature is  low ,you have to visit the doctor.";
+            }
+           
+        }
+    }
+}
